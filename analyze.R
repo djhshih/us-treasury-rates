af09724e1c970813bca7f5226183485490b4d0d3
David J. H. Shih <djh.shih@gmail.com>
2023-05-16 22:31:24 +0800

    Initial commit.

---
+++ analyze.R
@@ -0,0 +1,33 @@
+library(io)
+library(ggplot2)
+library(lubridate)
+library(tidyr)
+
+xs <- qread("data", pattern="daily-treasury-rates_.*.csv");
+
+x <- do.call(rbind, xs);
+colnames(x) <- tolower(colnames(x));
+x$date <- mdy(x$date);
+x <- x[order(x$date), ];
+
+d <- pivot_longer(x, !date, names_to="term", values_to="yield");
+d$term <- factor(d$term, levels=colnames(x)[-1]);
+
+qdraw(
+	ggplot(d, aes(x = date, y = yield)) +
+		facet_wrap(~ term, ncol=1, scales="free_y", strip.position="right") +
+		theme_classic() +
+		theme(
+			strip.background = element_blank(),
+			strip.text.y.right = element_text(angle=0),
+			panel.grid.minor.x = element_line(),
+			panel.grid.major.x = element_line()
+		) +
+		geom_line() +
+		geom_vline(xintercept=ymd("2011-08-02"), colour="firebrick") +
+		xlab("time") + ylab("US treasury yield")
+	,
+	width = 8, height = 10,
+	file = "us-treasury-yield_2011_debt-ceiling.pdf"
+)
+
---
+++ get.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Download US treasury rates
+
+set -euo pipefail
+IFS=$'\n\t'
+
+outdir=data
+mkdir -p $outdir
+
+get_treasury() {
+	year=$1
+	curl -o $outdir/daily-treasury-rates_$year.csv "https://home.treasury.gov/resource-center/data-chart-center/interest-rates/daily-treasury-rates.csv/$year/all?type=daily_treasury_yield_curve&field_tdr_date_value=$year&page&_format=csv"
+}
+
+years=( 2010 2011 2012 )
+
+for year in ${years[@]}; do
+	get_treasury $year
+done
